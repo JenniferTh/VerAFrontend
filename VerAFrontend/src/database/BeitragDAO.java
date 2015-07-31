e113b70be07f3f23add17f65599897b38d2529dc
Malte Lingrön <mlingroen@stud.hs-bremen.de>
2015-07-31 15:25:41 +0200

    Krams halt

--- VerAFrontend/src/database/BeitragDAO.java
+++ VerAFrontend/src/database/BeitragDAO.java
@@ -1,6 +1,9 @@
 package database;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.*;
 
 /**
  * Created by Malte on 06.07.2015.
@@ -13,12 +16,14 @@ public class BeitragDAO {
     PreparedStatement createArticle;
     PreparedStatement updateArticle;
     PreparedStatement searchArticle;
+    PreparedStatement getArticleFromUser;
 
 
     String sqlCreateArticle;
     String sqlDeleteArticle;
     String sqlUpdateArticle;
     String sqlSearchArticle;
+    String sqlGetArticleFromUser;
     
     public BeitragDAO(){
         connection = new DBConnection().getConnection();
@@ -31,11 +36,13 @@ public class BeitragDAO {
         sqlDeleteArticle = "DELETE FROM Beitrag WHERE Beitrag_ID =?";
         sqlUpdateArticle = "UPDATE Beitrag SET Titel=?, Inhalt = ? where Beitrag_ID = ? ";
         sqlSearchArticle = "SELECT * FROM Beitrag where Beitrag.Titel like %?% or Beitrag.Titel = %?%";
+        sqlGetArticleFromUser ="Select * FROM Beitrag WHERE Mitgliedsnummer =?";
         try {
             this.createArticle = this.connection.prepareStatement(sqlCreateArticle);
             this.deleteArticle = this.connection.prepareStatement(sqlDeleteArticle);
             this.updateArticle = this.connection.prepareStatement(sqlUpdateArticle);
             this.searchArticle = this.connection.prepareStatement(sqlSearchArticle);
+            this.getArticleFromUser = this.connection.prepareStatement(sqlGetArticleFromUser);
 
         } catch (SQLException e) {
             System.out.println("Error while creating prepared Statements");
@@ -81,19 +88,54 @@ public class BeitragDAO {
         }
     }
     
-    public ResultSet searchArticle(String term){
-    	ResultSet resSet;
+    public List<Article> searchArticle(String term){
+    	ResultSet articleRS;
+		List<Article> articleList = new ArrayList<Article>();
+
+    	
     	try {
         	searchArticle.setString(1, term);
     		searchArticle.setString(2, term);
-    		resSet = searchArticle.executeQuery();
-    		return resSet;
+    		articleRS = searchArticle.executeQuery();
+    		
+			while(articleRS.next()){
+				String title = articleRS.getString(2);
+				String category = articleRS.getString(3);
+				String content= articleRS.getString(4);
+				int userID  = articleRS.getInt(6);
+				articleList.add(new Article(title, category, content, userID));
+			}
+    		
+    		return articleList;
     		
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			resSet = null;
-			return resSet;
+			return null;
+		}
+    }
+    
+    public List<Article> getArticleFromUser(int mitgliedsnummer){
+    	try {
+    		ResultSet articleFromUserRS;
+    		List<Article> articleFromUserList = new ArrayList<Article>();
+    		
+			getArticleFromUser.setInt(1, mitgliedsnummer);
+			articleFromUserRS = getArticleFromUser.executeQuery();
+			
+			while(articleFromUserRS.next()){
+				String title = articleFromUserRS.getString(2);
+				String category = articleFromUserRS.getString(3);
+				String content= articleFromUserRS.getString(4);
+				int userID  = articleFromUserRS.getInt(6);
+				articleFromUserList.add(new Article(title, category, content, userID));
+			}
+			
+			return articleFromUserList;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
 		}
     }
 
