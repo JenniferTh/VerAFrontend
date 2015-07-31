ab068da522218e25294dc25790880f4593f5dae8
JenniferTh <J-Theloy@gmx.de>
2015-07-31 12:59:14 +0200

    Treffen
    Signed-off-by: JenniferTh <J-Theloy@gmx.de>

---
+++ VerAFrontend/src/action/meetingListing.java
@@ -0,0 +1,36 @@
+package action;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+import com.opensymphony.xwork2.ActionSupport;
+import model.Treffen;
+
+public class meetingListing extends ActionSupport implements SessionAware{
+	List<Treffen> meetings;
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		
+		return SUCCESS;
+	}
+	
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Treffen> getMeetings() {
+		return meetings;
+	}
+
+	public void setMeetings(List<Treffen> meetings) {
+		this.meetings = meetings;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
---
+++ VerAFrontend/src/model/Treffen.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Treffen {
+	private String thema, info, kategorie, ort, uhrzeit;
+	private int maxTeilnehmer;
+
+	
+	public String getThema() {
+		return thema;
+	}
+	public void setThema(String thema) {
+		this.thema = thema;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getKategorie() {
+		return kategorie;
+	}
+	public void setKategorie(String kategorie) {
+		this.kategorie = kategorie;
+	}
+	public String getUhrzeit() {
+		return uhrzeit;
+	}
+	public void setUhrzeit(String uhrzeit) {
+		this.uhrzeit = uhrzeit;
+	}
+	public int getMaxTeilnehmer() {
+		return maxTeilnehmer;
+	}
+	public void setMaxTeilnehmer(int maxTeilnehmer) {
+		this.maxTeilnehmer = maxTeilnehmer;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+}
